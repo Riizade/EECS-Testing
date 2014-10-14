aa12d0b788386962c4a75281049a632a8f43813c
Riizade <riizade@gmail.com>
2014-10-14 16:45:17 -0400

    refactored execution path, but left old code in

--- test.py
+++ test.py
@@ -22,7 +22,7 @@ def main(argv):
     arguments_list = ""
     diff = 0
     n = -1
-    mode = -1
+    outmode = ""
     suffix = ""
 
     print("\nThis is a test suite for EECS classes at University of Michigan built by Adam Johnson.\n")
@@ -56,26 +56,19 @@ def main(argv):
         elif opt in ("-s", "--suffix"):
             suffix = arg
         elif opt in ("-o", "--out"):
-            if arg == "gen":
-                mode = 1
-            elif arg == "test":
-                mode = 0
-            elif arg == "print":
-                mode = 3
-            elif arg == "rm":
-                mode = 2
+            outmode = arg
 
-    #decide which mode to use
-    if mode == 0:
+    #decide which output mode to use
+    if outmode == "test":
         run_tests(executable_name, arguments_list, diff, suffix, n)
         sys.exit()
-    elif mode == 1:
+    elif outmode == "gen":
         rename(executable_name, arguments_list, suffix)
         sys.exit()
-    elif mode == 2:
+    elif outmode == "rm":
         remove(suffix)
         sys.exit()
-    elif mode == 3:
+    elif outmode == "print":
         run_simple(executable_name, arguments_list, n)
         sys.exit()
     else:
@@ -94,7 +87,7 @@ def usage():
     print("")
     print("The commands are as follows:")
     print("\t-h, --help:\t\tPrints this help text.")
-    print("\t-n, --num [arg]:\tSpecifies the number of lines to output.")
+    print("\t-n, --num [arg]:\tSpecifies the number of lines to output for each test case.")
     print("\t-a, --args \"[arg]\":\tSpecifies all of your program's command line arguments.")
     print("\t-e, --exe [arg]:\tSpecifies the name of your program executable when using a single executable build mode.")
     print("\tBuild Modes")
@@ -116,11 +109,12 @@ def usage():
     print("\tpython ./test.py -o test -s old -e exchange -a \"-v -m -t\" -d")
 
 
-def build(executable_name):
+# runs the command ("make "+make_target)
+def build(make_target):
     #build the binary
     print("Building the executable:")
-    print('make -r -R')
-    out = commands.getoutput('make -r -R')
+    print('make '+make_target)
+    out = commands.getoutput('make '+make_target)
     print(out)
     print('')
     #if an error occurs during building
@@ -131,15 +125,16 @@ def build(executable_name):
         print('Build successful.')
 
 
+# runs make clean
 def clean():
-        #clean up after itself
-        print('Removing executable and objects:')
-        print('make clean')
-        os.system('make clean')
+    #clean up after itself
+    print('Cleaning up:')
+    print('make clean')
+    os.system('make clean')
 
 
 def rename(executable_name, arguments_list, suffix):
-    build(executable_name)
+    build("")
 
     filenames = os.listdir('./')
     filenames.sort()
@@ -153,6 +148,7 @@ def rename(executable_name, arguments_list, suffix):
             os.system('./' + executable_name + ' ' + arguments_list + ' < ' + filename + ' > ' + filename[:-4] + '_' + suffix + '.txt')
 
 
+# deletes test outputs with the correct suffix
 def remove(suffix):
     print('Removing _' + suffix + '.txt files:')
     filenames = os.listdir('./')
@@ -164,9 +160,90 @@ def remove(suffix):
     print('')
 
 
+# returns a list of the filenames of the testcases
+def get_testcases():
+    filenames = os.listdir('./')
+    testnames = []
+    for filename in filenames:
+        if filename[0:5] == "test-" and not '_' in filename:
+            testnames.append(filename)
+
+    testnames.sort()
+    return testnames
+
+
+# returns a list of files that have outputs with the correct suffix for a test case
+def get_outputfiles(suffix):
+    files = []
+    for test in get_testcases():
+        if os.path.isfile("./" + test[:-4] + '_' + suffix + '.txt'):
+            files.append("./" + test[:-4] + '_' + suffix + '.txt')
+
+    files.sort()
+    return files
+
+
+# returns a dictionary where keys are test names and values are the outputs of those tests
+def run_tests_text(executable_name, arguments_list):
+    build("")
+
+    tests = get_testcases()
+    outputs = {}
+
+    print("Running ./" + executable_name + " " + arguments_list + "...")
+
+    for test in tests:
+        out = commands.getoutput('./' + executable_name + ' ' + arguments_list + ' < ' + test)
+        outputs[test] = out
+
+    clean()
+
+    return outputs
+
+
+# returns a dictionary where keys are test names and values are the outputs of those tests
+def run_tests_make(arguments_list):
+    outputs = {}
+
+    # get test cases
+    for test in get_testcases():
+        # if it's a .cpp file
+        if os.path.isfile("./" + test[:-4] + '.cpp'):
+            # build the make target
+            build(test[:-4])
+            # run the executable
+            out = commands.getoutput('./' + test[:-4] + ' ' + arguments_list)
+            outputs[test] = out
+
+    clean()
+
+    return outputs
+
+
+# prints test outputs
+# optionally limits the number of lines per test output
+def print_outputs(outputs, n):
+    for test, output in outputs:
+        print(divider())
+        print("Output for " + test + ":")
+        print(shorten_output(output, n))
+
+    print(divider())
+
+
+# if the output string has lines > numlines, it will be truncated to be only numlines lines
+def shorten_output(output, numlines):
+    # if there's no limit
+    if numlines == -1:
+        return output
+    else:
+        outlines = out.split("\n")
+        return "\n".join(outlines[:numlines])
+
+
 def run_simple(executable_name, arguments_list, numlines):
 
-    build(executable_name)
+    build("")
 
     filenames = os.listdir('./')
     filenames.sort()
@@ -192,9 +269,83 @@ def run_simple(executable_name, arguments_list, numlines):
     clean()
 
 
+def compare_outputs(executable_name, arguments_list, diff, suffix, numlines):
+
+    build("")
+
+    #create structures
+    tests_passed = []
+    tests_failed = []
+    tests_no_sln = []
+
+    tests = get_testcases()
+
+    print("Testing ./" + executable_name + " " + arguments_list + "...")
+
+
+
+    print('Comparing output...')
+    for test in tests:
+        #if a solution exists
+        if os.path.isfile("./" + test[:-4] + '_' + suffix + '.txt'):
+            #run the program
+            os.system('./' + executable_name + ' ' + arguments_list + ' < ' + filename + ' > ' + filename[:-4] + '_tmp.txt')
+            #diff the output
+            out = commands.getoutput('diff ' + filename[:-4] + '_tmp.txt ' + filename[:-4] + '_' + suffix + '.txt')
+            if out == '':
+                tests_passed.append(filename)
+            else:
+                tests_failed.append((filename, out))
+        else:
+            #alert user that no solution exists
+            tests_no_sln.append(filename)
+
+    print('\nTests Passed:')
+    if len(tests_passed) != 0:
+        tests_passed.sort()
+        for test in tests_passed:
+            print(test)
+    else:
+        print('None')
+
+    print('\nTests with no _' + suffix + '.txt:')
+    if len(tests_no_sln) != 0:
+        tests_no_sln.sort()
+        for test in tests_no_sln:
+            print(test)
+    else:
+        print('None')
+
+    print('\nTests Failed:')
+    if len(tests_failed) != 0:
+        tests_failed.sort()
+        for test in tests_failed:
+            print(test[0])
+            if (diff == 1):
+                print(divider())
+                if (numlines == -1):
+                    print(test[1])
+                else:
+                    outlines = test[1].split("\n")
+                    for i in range(len(outlines)):
+                        if (i > numlines):
+                            break
+                        print(outlines[i])
+            if (diff == 1):
+                print(divider())
+    else:
+        print('None')
+    print('')
+
+    remove('tmp')
+
+    clean()
+
+
+
 def run_tests(executable_name, arguments_list, diff, suffix, numlines):
 
-    build(executable_name)
+    build("")
 
     #create structures
     tests_passed = []
