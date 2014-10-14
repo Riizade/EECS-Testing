bcc0d0d0ff18ea557999285fbe5fcc03ef68c292
Riizade <riizade@gmail.com>
2014-10-14 17:29:47 -0400

    script now uses new execution path. completely untested

--- test.py
+++ test.py
@@ -17,7 +17,8 @@ def divider():
 
 
 def main(argv):
-    build_mode = ""
+    # argument variables
+    buildmode = ""
     executable_name = ""
     arguments_list = ""
     diff = 0
@@ -45,7 +46,7 @@ def main(argv):
         elif opt in ("-e", "--exe"):
             executable_name = arg
         elif opt in ("-b", "--build"):
-            build_mode = arg
+            buildmode = arg
         elif opt in ("-a", "--args"):
             arguments_list = arg
             arguments_list.strip('\"')
@@ -58,18 +59,21 @@ def main(argv):
         elif opt in ("-o", "--out"):
             outmode = arg
 
-    #decide which output mode to use
+    # decide which output mode to use
     if outmode == "test":
-        run_tests(executable_name, arguments_list, diff, suffix, n)
+        outputs = run_tests(executable_name, arguments_list, buildmode)
+        compare_outputs(outputs, suffix, diff)
         sys.exit()
     elif outmode == "gen":
-        rename(executable_name, arguments_list, suffix)
+        outputs = run_tests(executable_name, arguments_list, buildmode)
+        generate_files(outputs, suffix)
         sys.exit()
     elif outmode == "rm":
         remove(suffix)
         sys.exit()
     elif outmode == "print":
-        run_simple(executable_name, arguments_list, n)
+        outputs = run_tests(executable_name, arguments_list, buildmode)
+        print_outputs(outputs, n)
         sys.exit()
     else:
         usage()
@@ -89,10 +93,10 @@ def usage():
     print("\t-h, --help:\t\tPrints this help text.")
     print("\t-n, --num [arg]:\tSpecifies the number of lines to output for each test case.")
     print("\t-a, --args \"[arg]\":\tSpecifies all of your program's command line arguments.")
-    print("\t-e, --exe [arg]:\tSpecifies the name of your program executable when using a single executable build mode.")
+    print("\t-e, --exe [arg]:\tSpecifies the name of your program executable if using a single executable build mode.")
     print("\tBuild Modes")
     print("\t-b, --build [arg]:\tSpecifies the build and execution mode.")
-    print("\t\t\"txt\":\tOne executable already built, each test case is a text file read to stdin.")
+    print("\t\t\"text\":\tOne executable already built, each test case is a text file read to stdin.")
     print("\t\t\"make\":\tEach test case is a cpp file with its own make target.")
     print("\tOutput Modes")
     print("\t-s, --suffix [arg]:\t\tSpecifies the suffix to be used in the output mode.")
@@ -104,9 +108,9 @@ def usage():
     print("\t\t\"print\":\tPrints current program output for each test to console.")
     print("")
     print("A valid example of generating test-*_old.txt files:")
-    print("\tpython ./test.py -o gen -s old -e exchange -a \"-v -m -t\"")
+    print("\tpython ./test.py -b text -o gen -s old -e exchange -a \"-v -m -t\"")
     print("A valid example of regression testing against test-*_old.txt files:")
-    print("\tpython ./test.py -o test -s old -e exchange -a \"-v -m -t\" -d")
+    print("\tpython ./test.py -b text -o test -s old -e exchange -a \"-v -m -t\" -d")
 
 
 # runs the command ("make "+make_target)
@@ -133,6 +137,14 @@ def clean():
     os.system('make clean')
 
 
+def generate_files(outputs, suffix):
+    for testname, output in outputs:
+        filename = testname+"_"+suffix+".txt"
+        f = open(filename)
+        f.write(output)
+        f.close()
+
+
 def rename(executable_name, arguments_list, suffix):
     build("")
 
@@ -145,7 +157,7 @@ def rename(executable_name, arguments_list, suffix):
         if filename[0:5] == "test-" and not '_' in filename:
             #if a solution exists
             #run the program
-            os.system('./' + executable_name + ' ' + arguments_list + ' < ' + filename + ' > ' + filename[:-4] + '_' + suffix + '.txt')
+            os.system('./'+executable_name+' '+arguments_list+' < '+filename+' > '+rm_ext(filename)+'_'+suffix+'.txt')
 
 
 # deletes test outputs with the correct suffix
@@ -172,15 +184,33 @@ def get_testcases():
     return testnames
 
 
+# returns a version of the string filename with the file extension truncated
+def rm_ext(filename):
+    return os.path.splitext(filename)[0]
+
+
 # returns a list of files that have outputs with the correct suffix for a test case
 def get_outputfiles(suffix):
     files = []
     for test in get_testcases():
-        if os.path.isfile("./" + test[:-4] + '_' + suffix + '.txt'):
-            files.append("./" + test[:-4] + '_' + suffix + '.txt')
+        if os.path.isfile("./" + rm_ext(test) + '_' + suffix + '.txt'):
+            files.append("./" + rm_ext(test) + '_' + suffix + '.txt')
 
     files.sort()
     return files
+
+
+# returns a dictionary where keys are test names and values are the outputs of those tests
+def run_tests(executable_name, arguments_list, buildmode):
+    # decide which build mode to use
+    if buildmode == "text":
+        return run_tests_text(executable_name, arguments_list)
+    elif buildmode == "make":
+        return run_tests_make(arguments_list)
+    else:
+        usage()
+        print("Error: No valid build mode specified.")
+        sys.exit(2)
 
 
 # returns a dictionary where keys are test names and values are the outputs of those tests
@@ -206,14 +236,14 @@ def run_tests_make(arguments_list):
     outputs = {}
 
     # get test cases
-    for test in get_testcases():
+    for testname in get_testcases():
         # if it's a .cpp file
-        if os.path.isfile("./" + test[:-4] + '.cpp'):
+        if os.path.isfile("./" + rm_ext(testname) + '.cpp'):
             # build the make target
-            build(test[:-4])
+            build(rm_ext(testname))
             # run the executable
-            out = commands.getoutput('./' + test[:-4] + ' ' + arguments_list)
-            outputs[test] = out
+            out = commands.getoutput('./' + rm_ext(testname) + ' ' + arguments_list)
+            outputs[rm_ext(testname)] = out
 
     clean()
 
@@ -223,9 +253,9 @@ def run_tests_make(arguments_list):
 # prints test outputs
 # optionally limits the number of lines per test output
 def print_outputs(outputs, n):
-    for test, output in outputs:
+    for testname, output in outputs:
         print(divider())
-        print("Output for " + test + ":")
+        print("Output for " + testname + ":")
         print(shorten_output(output, n))
 
     print(divider())
@@ -269,29 +299,20 @@ def run_simple(executable_name, arguments_list, numlines):
     clean()
 
 
-def compare_outputs(executable_name, arguments_list, diff, suffix, numlines):
+def compare_outputs(outputs, suffix, diff):
 
-    build("")
-
-    #create structures
+    # create data structures
     tests_passed = []
     tests_failed = []
     tests_no_sln = []
 
-    tests = get_testcases()
-
-    print("Testing ./" + executable_name + " " + arguments_list + "...")
-
-
+    generate_files(outputs, "tmp")
 
     print('Comparing output...')
-    for test in tests:
-        #if a solution exists
-        if os.path.isfile("./" + test[:-4] + '_' + suffix + '.txt'):
-            #run the program
-            os.system('./' + executable_name + ' ' + arguments_list + ' < ' + filename + ' > ' + filename[:-4] + '_tmp.txt')
-            #diff the output
-            out = commands.getoutput('diff ' + filename[:-4] + '_tmp.txt ' + filename[:-4] + '_' + suffix + '.txt')
+    for test, output in outputs:
+        if os.path.isfile(test+"_"+suffix+".txt"):
+            # diff the output
+            out = commands.getoutput('diff ' + test + '_tmp.txt ' + test + '_' + suffix + '.txt')
             if out == '':
                 tests_passed.append(filename)
             else:
@@ -320,19 +341,12 @@ def compare_outputs(executable_name, arguments_list, diff, suffix, numlines):
     if len(tests_failed) != 0:
         tests_failed.sort()
         for test in tests_failed:
+            # print name
             print(test[0])
-            if (diff == 1):
+            # print diff
+            if diff == 1:
                 print(divider())
-                if (numlines == -1):
-                    print(test[1])
-                else:
-                    outlines = test[1].split("\n")
-                    for i in range(len(outlines)):
-                        if (i > numlines):
-                            break
-                        print(outlines[i])
-            if (diff == 1):
-                print(divider())
+                print(shorten_output(test[1], n))
     else:
         print('None')
     print('')
@@ -340,81 +354,3 @@ def compare_outputs(executable_name, arguments_list, diff, suffix, numlines):
     remove('tmp')
 
     clean()
-
-
-
-def run_tests(executable_name, arguments_list, diff, suffix, numlines):
-
-    build("")
-
-    #create structures
-    tests_passed = []
-    tests_failed = []
-    tests_no_sln = []
-
-    filenames = os.listdir('./')
-    filenames.sort()
-
-    print("Testing ./" + executable_name + " " + arguments_list + "...")
-
-    print('Comparing output...')
-    for filename in filenames:
-        #for each test case
-        if filename[0:5] == "test-" and not '_' in filename:
-            #if a solution exists
-            if os.path.isfile("./" + filename[:-4] + '_' + suffix + '.txt'):
-                #run the program
-                os.system('./' + executable_name + ' ' + arguments_list + ' < ' + filename + ' > ' + filename[:-4] + '_tmp.txt')
-                #diff the output
-                out = commands.getoutput('diff ' + filename[:-4] + '_tmp.txt ' + filename[:-4] + '_' + suffix + '.txt')
-                if out == '':
-                    tests_passed.append(filename)
-                else:
-                    tests_failed.append((filename, out))
-            else:
-                #alert user that no solution exists
-                tests_no_sln.append(filename)
-
-    print('\nTests Passed:')
-    if len(tests_passed) != 0:
-        tests_passed.sort()
-        for test in tests_passed:
-            print(test)
-    else:
-        print('None')
-
-    print('\nTests with no _' + suffix + '.txt:')
-    if len(tests_no_sln) != 0:
-        tests_no_sln.sort()
-        for test in tests_no_sln:
-            print(test)
-    else:
-        print('None')
-
-    print('\nTests Failed:')
-    if len(tests_failed) != 0:
-        tests_failed.sort()
-        for test in tests_failed:
-            print(test[0])
-            if (diff == 1):
-                print(divider())
-                if (numlines == -1):
-                    print(test[1])
-                else:
-                    outlines = test[1].split("\n")
-                    for i in range(len(outlines)):
-                        if (i > numlines):
-                            break
-                        print(outlines[i])
-            if (diff == 1):
-                print(divider())
-    else:
-        print('None')
-    print('')
-
-    remove('tmp')
-
-    clean()
-
-if __name__ == "__main__":
-    main(sys.argv)
